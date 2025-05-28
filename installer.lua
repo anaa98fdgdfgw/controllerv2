4df665fffed80077e28da619f543438e21d64264
anaa98fdgdfgw <anaisamano.resident@gmail.com>
2025-05-28 20:49:57 +0200

    Create installer.lua

---
+++ installer.lua
@@ -0,0 +1,8 @@
+shell.run("wget", "https://raw.githubusercontent.com/anaa98fdgdfgw/controller/refs/heads/main/CC/main.lua", "main.lua")
+
+-- Téléchargement de Basalt 2 (officiel)
+print("[Installer] Téléchargement de Basalt 2 (UI)...")
+shell.run("wget run https://raw.githubusercontent.com/Pyroxenium/Basalt2/main/install.lua -r")
+print("[Installer] Basalt 2 installé avec succès !")
+
+print("Installation terminée ! Utilise la commande main pour démarrer.")
